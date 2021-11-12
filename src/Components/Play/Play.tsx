9b5062c03ca2636fbb3c0acbd3ff823ab42ea6a9
George Yu <>
2021-11-11 18:30:53 -0800

    added howler and plays sequentially

--- src/Components/Play/Play.tsx
+++ src/Components/Play/Play.tsx
@@ -10,7 +10,8 @@ interface Song {
 export interface SongData {
   songs: Song[];
 }
-const SAVED_TRACKS_ENDPOINT = "https://api.spotify.com/v1/me/tracks";
+const SAVED_TRACKS_ENDPOINT =
+  "https://api.spotify.com/v1/me/tracks?limit=30&offset=0";
 const Play: React.FC = () => {
   const [songList, setSongList] = useState<Song[]>([]);
   const handleArtist = (artists: any[]): string[] => {
@@ -20,18 +21,28 @@ const Play: React.FC = () => {
     });
     return aristList;
   };
+
+  function shuffleArray(array: Song[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
   useEffect(() => {
     if (sessionStorage.getItem("accessToken")) {
       const token = sessionStorage.getItem("accessToken");
-      console.log(sessionStorage.getItem("accessToken"));
-      sendSongListRequest(token);
+      sendSongListRequest(token, SAVED_TRACKS_ENDPOINT);
     }
     return () => {};
   }, []);
 
-  const sendSongListRequest = (token: string | null) => {
+  const sendSongListRequest = (
+    token: string | null,
+    endpoint: string
+  ): null | string => {
     axios
-      .get(SAVED_TRACKS_ENDPOINT, {
+      .get(endpoint, {
         headers: {
           Authorization: "Bearer " + token,
         },
@@ -42,11 +53,13 @@ const Play: React.FC = () => {
           const newSong: Song = parseSong(song);
           sl.push(newSong);
         });
-        setSongList(sl);
+        setSongList(sl.concat(songList));
       })
       .catch((err) => {
         console.log(err);
+        return null;
       });
+    return null;
   };
   const parseSong = (song: any): Song => {
     const newSong: Song = {
@@ -59,27 +72,35 @@ const Play: React.FC = () => {
   };
   return (
     <React.Fragment>
-      <MusicPlayer songs={songList} />
+      <MusicPlayer songs={shuffleArray(songList)} />
     </React.Fragment>
   );
 };
 
 const MusicPlayer = (data: SongData) => {
   const [audioState, setAudioState] = useState<boolean>(false);
+  let [songIndex, setIndex] = useState<number>(0);
   if (data.songs.length == 0) {
     return <></>;
   } else {
     return (
       <React.Fragment>
-        {console.log(data.songs[0].previewUrl + ".mp3")}
+        {console.log(data.songs[songIndex].previewUrl + ".mp3")}
         <ReactHowler
-          src={data.songs[0].previewUrl}
+          src={data.songs[songIndex].previewUrl}
           format={["mp3"]}
           html5={true}
           playing={audioState}
+          onEnd={() => setIndex((songIndex = songIndex + 1))}
         />
+        <header>
+          {songIndex + 1} / {data.songs.length}
+        </header>
         <button onClick={() => setAudioState(!audioState)}>
           {audioState ? "Pause" : "Play"}
+        </button>
+        <button onClick={() => setIndex((songIndex = songIndex + 1))}>
+          Skip
         </button>
       </React.Fragment>
     );
