5e58ab76c79b4e100fd098546b926377ac19ef5f
George Yu <>
2021-11-17 11:23:32 -0800

    styled result page

--- src/Components/Results/Results.tsx
+++ src/Components/Results/Results.tsx
@@ -2,7 +2,7 @@ import { useLocation } from "react-router-dom";
 import React from "react";
 import { Song } from "../Play/Play";
 import MusicNotes from "../MusicNotes/MusicNotes";
-
+import "./Results.css";
 interface ResultProps {
   score: number;
   songs: Song[];
@@ -12,8 +12,31 @@ const Results = () => {
   const props: any = location.state;
   return (
     <React.Fragment>
-      <MusicNotes></MusicNotes>
-      <div>{props.score}</div>
+      <div className="result-bg">
+        <div className="result-title">
+          You Scored <text style={{ color: "skyblue" }}>{props.score}</text> out
+          of {props.songs.length}
+          <p> Here are the songs you played:</p>
+          <table className="song-table">
+            <tr>
+              <th> Album </th>
+              <th> Song </th>
+              <th> Artist </th>
+            </tr>
+            {props.songs.map((song: Song) => {
+              return (
+                <tr className="song-entry">
+                  <th>
+                    <img src={song.albumImgUrl}></img>
+                  </th>
+                  <th>{song.songName}</th>
+                  <th>{song.artist.join(", ")}</th>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
